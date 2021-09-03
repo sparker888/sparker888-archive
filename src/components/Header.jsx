1c425560f86557ebe24307fd8804ab352428619a
Stephen Parker <sparker888@gmail.com>
2021-09-03 05:34:05 -0400

    wip:nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import tw, {styled} from "twin.macro"
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "gatsby";
 import { MenuIcon } from "@heroicons/react/solid"
 
@@ -9,51 +9,58 @@ const NavBar = tw.nav`
 const Wrapper = tw.div`
   container flex flex-col sm:flex-row items-center justify-center p-6 mx-auto text-gunmetal capitalize
 `
-const MenuLink = tw(props => <Link {...props}/>)`
-  text-gunmetal hover: text-burnt mx-1.5 sm:mx-6
+const NavMenu = styled.div`
+  ${show ? "display:none" : "display:block" }
 `
-const InactiveIcon = tw(props => <MenuIcon {...props}/>)`
-  opacity-0 w-10 h-10
-`
-const ActiveIcon = tw(props => <MenuIcon {...props}/>)`
-  opacity-100 w-10 h-10
-`
-const ActiveLink = tw(props => <Link {...props}/>)`
-  text-gunmetal border-b-2 border-burnt mx-1.5 sm:mx-6
+const LinkIcon = tw(props => <MenuIcon {...props}/>)`
+  w-10 h-10 sm:opacity-100 md:opacity-0
 `
 const StyledLink = tw(props => <Link {...props}/>)`
   border-b-2 border-transparent text-gunmetal-light hover:text-gunmetal mx-1.5 sm:mx-6
 `
 
+
 export default function Header() {
+  
+  const [show, setShow] = useState(true);
+
   return (
     <NavBar>
       <Wrapper>
+        <NavMenu>
+          {/* Mobile menu button */}
+          <LinkIcon  onClick={() => setShow(!show)}/>
 
-        {/* Mobile menu button */}
-        <MenuLink>
-        <InactiveIcon />
-        </MenuLink>
+          <StyledLink to="/"
+            activeClassName="active-link"
+            onClick={() => setShow(false)}>
+            Stephen Parker
+          </StyledLink>
 
-        <StyledLink to="/">
-          Stephen Parker
-        </StyledLink>
+          <StyledLink to="/daily-photo"
+            activeClassName="active-link"
+            onClick={() => setShow(false)}>
+            Daily Photo
+          </StyledLink>
 
-        <StyledLink to="/daily-photo">
-          Daily Photo
-        </StyledLink>
+          <StyledLink to="/skilltree"
+            activeClassName="active-link"
+            onClick={() => setShow(false)}>
+            Skill Tree
+          </StyledLink>
 
-        <StyledLink to="/skilltree">
-          Skill Tree
-        </StyledLink>
+          <StyledLink to="/projects"
+            activeClassName="active-link"
+            onClick={() => setShow(false)}>
+            Projects
+          </StyledLink>
 
-        <StyledLink to="/projects">
-          Projects
-        </StyledLink>
-
-        <StyledLink to="/blog">
-          My Blog
-        </StyledLink>
+          <StyledLink to="/blog"
+            activeClassName="active-link"
+            onClick={() => setShow(false)}>
+            My Blog
+          </StyledLink>
+        </NavMenu>
       </Wrapper>
     </NavBar>
   );
