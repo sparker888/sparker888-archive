435f02e0088664fd39bc1e55bc47a7eda9fdfa32
Stephen Parker <sparker888@gmail.com>
2021-09-04 18:46:49 -0400

    start of new nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,20 @@
 import tw, {styled} from "twin.macro";
 import React, { useState } from "react";
-import { Link } from "gatsby";
+import { Link } from "gatsby"
 import { MenuIcon } from "@heroicons/react/solid";
 
-const NavBar = styled.nav`
-${tw`bg-white shadow`};
-`
-const Wrapper = styled.div`
-${tw`container`};
-`
-const Menu = styled.div`
-${tw`flex flex-col md:flex-row items-center justify-center p-6 mx-auto text-gunmetal capitalize`};
-`
-const MenuLink = styled.div`
-${tw`text-gunmetal hover: text-burnt mx-1.5 sm:mx-6`};
-`
-const IconButton = styled(MenuIcon)`
-${tw`opacity-100 md:opacity-0 w-10 h-10`};
-`
-const StyledLink = styled(Link)`
-${tw`border-b-2 border-transparent text-gunmetal-light hover:text-gunmetal mx-1.5 sm:mx-6`};
-`
-export default function Header() {
 
-  const [clickedState, setClickedState] = useState(false);
-  const menuClick = () => {
-    setClickedState(!clickedState);
-  };
+export default function Header() {
 
   return (
 
-    <NavBar>
-      <Wrapper>
-        <Menu>
+    <Wrapper>
+      <InnerWrapper>
+        <div>
 
-          {/* Mobile menu button */}
-          <MenuLink>
-          <IconButton onClick={menuClick}/>
-          </MenuLink>
+          <IconButton>
+            <button></button>
+          </IconButton>  
 
           <StyledLink to="/"
             activeStyle={tw`text-gunmetal border-b-2 border-burnt mx-1.5 sm:mx-6`}>
@@ -63,9 +40,22 @@ export default function Header() {
             activeStyle={tw`text-gunmetal border-b-2 border-burnt mx-1.5 sm:mx-6`}>
             My Blog
           </StyledLink>
-        </Menu>  
-      </Wrapper>
-    </NavBar>
 
+        </div>
+      </InnerWrapper>  
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+${tw`container`};
+`
+const InnerWrapper = styled.div`
+${tw`container`};
+`
+const StyledLink = styled(Link)`
+${tw`border-b-2 border-transparent text-gunmetal-light hover:text-gunmetal mx-1.5 sm:mx-6`};
+`
+const IconButton = styled(MenuIcon)`
+${tw`opacity-100 w-10 h-10`};
+`
