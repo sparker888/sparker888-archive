514e7fc089afdf23d5749bc7c6b61c4fe47dd288
Stephen Parker <sparker888@gmail.com>
2021-09-05 16:01:07 -0400

    links logic complete

---
+++ src/components/LinksMobile.jsx
@@ -0,0 +1,41 @@
+import tw, {styled} from "twin.macro";
+import React from 'react'
+import { Link } from "gatsby"
+
+export default function LinksMobile() {
+
+    return (
+    <>
+        <StyledLink to="/"
+            activeStyle={tw`block mt-1 px-2 py-1 font-semibold text-burnt rounded border-b-2 border-burnt`}>
+            Stephen Parker
+        </StyledLink>
+
+        <StyledLink to="/daily-photo"
+            activeStyle={tw`block mt-1 px-2 py-1 font-semibold text-burnt rounded border-b-2 border-burnt`}>
+            Daily Photo
+        </StyledLink>
+
+        <StyledLink to="/skilltree"
+            activeStyle={tw`block mt-1 px-2 py-1 font-semibold text-burnt rounded border-b-2 border-burnt`}>
+            Skill Tree
+        </StyledLink>
+
+        <StyledLink to="/projects"
+            activeStyle={tw`block mt-1 px-2 py-1 font-semibold text-burnt rounded border-b-2 border-burnt`}>
+            Projects
+        </StyledLink>
+
+        <StyledLink to="/blog"
+            activeStyle={tw`block mt-1 px-2 py-1 font-semibold text-burnt rounded border-b-2 border-burnt`}>
+            My Blog
+        </StyledLink>
+    </>
+    )
+}
+
+const StyledLink = styled(Link)`
+${tw`block sm:hidden mt-1 px-2 py-1 font-semibold text-gunmetal rounded hover:bg-powder`};
+`
+
+
