6402d18fd471c03426384201ada6d7a1de31be92
Stephen Parker <sparker888@gmail.com>
2021-09-16 12:16:58 -0400

    added gallery page

---
+++ src/pages/gallery.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from "../layouts/Layout";
+
+const Gallery = () => {
+  return (
+    <div>
+      <Layout>
+          <h1>Hello from the Gallery page.</h1>
+      </Layout>
+    </div>
+  )
+}
+
+export default Gallery
