342a8ecd4cc5af76f1bfc9fef23c5aa75b20533d
Stephen Parker <sparker888@gmail.com>
2021-09-20 14:06:42 -0400

    updates to skills page

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,16 +1,37 @@
-import React from 'react'
-import Layout from "../layouts/Layout";
+import tw, { styled } from "twin.macro"
+import React from "react"
+import Layout from "../layouts/Layout"
 import SkillsIntro from "../components/SkillsIntro"
 
 const Skills = () => {
   return (
     <div>
       <Layout>
-      <SkillsIntro />
-          <h1>Hello from the Skills page.</h1>
+        <SkillsIntro />
+        <Wrapper>
+          <InnerWrapper>
+            <ContentWrapper>
+              <Span>🚧THIS PAGE IS UNDER CONSTRUCTION🚧</Span>
+              <Span>This is where my skills will be listed, a contact form, and download resume button.</Span>
+            </ContentWrapper>
+          </InnerWrapper>
+        </Wrapper>
       </Layout>
     </div>
   )
 }
 
 export default Skills
+
+const Wrapper = tw.div`
+bg-almond
+`
+const InnerWrapper = tw.div`
+max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8
+`
+const ContentWrapper = tw.h2`
+text-3xl font-extrabold tracking-tight text-amber sm:text-4xl
+`
+const Span = tw.span`
+block
+`
