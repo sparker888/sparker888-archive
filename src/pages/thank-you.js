41f6d5f7fa0cfe3617279008e544d3bef517172a
Stephen Parker <sparker888@gmail.com>
2021-09-22 11:24:14 -0400

    adding polish to thank you ✨

---
+++ src/pages/thank-you.js
@@ -0,0 +1,68 @@
+import tw, { styled } from "twin.macro"
+import React from "react"
+import Layout from "../layouts/Layout"
+import { Link } from "gatsby"
+
+const ThankYou = () => {
+  return (
+    <Layout>
+      <OuterWrapper>
+        <InnerWrapper>
+          <MAIN>
+            <P>Thank you!</P>
+            <Content>
+              <ContentWrapper>
+                <H1>Your message has been received.</H1>
+                <P2>Where would you like to go from here?</P2>
+              </ContentWrapper>
+              <ButtonWrapper>
+                <Button1Style to="/" alt="home">
+                  Return home
+                </Button1Style>
+                {/* <a
+              href="#"
+              className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Contact support
+            </a> */}
+              </ButtonWrapper>
+            </Content>
+          </MAIN>
+        </InnerWrapper>
+      </OuterWrapper>
+    </Layout>
+  )
+}
+
+export default ThankYou
+
+const OuterWrapper = tw.div`
+bg-almond min-h-screen px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8
+`
+const InnerWrapper = tw.div`
+max-w-max mx-auto
+`
+const MAIN = tw.main`
+sm:flex
+`
+const P = tw.p`
+text-4xl font-extrabold text-kobe-light sm:text-5xl
+`
+const Content = tw.div`
+sm:ml-6
+`
+const ContentWrapper = tw.div`
+sm:border-l sm:border-ming sm:pl-6
+`
+const H1 = tw.h1`
+text-4xl font-extrabold text-fogra tracking-tight sm:text-5xl
+`
+const P2 = tw.p`
+mt-1 text-base text-fogra-lightest
+`
+const ButtonWrapper = tw.button`
+mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6
+`
+const Button1Style = styled(Link)`
+  ${tw`inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-kobe hover:bg-amber focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-dark`}
+`
