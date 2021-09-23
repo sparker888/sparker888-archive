da801f45cb0c4b4ee3c57d95d534999b28a5ed47
Stephen Parker <sparker888@gmail.com>
2021-09-23 17:25:31 -0400

    update files for mdx components

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,13 +2,23 @@ import React from "react";
 import { GlobalStyles } from "twin.macro";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { MDXProvider } from "@mdx-js/react"
+import { RegVideo, Video, Code, Blockquote, Counter } from "../components/shortcodes"
+
+const shortcodes = { RegVideo,
+  Video,
+  Code,
+  Blockquote,
+  Counter }
 
 const Layout = ({ children }) => {
   return (
     <>
       <GlobalStyles />
       <Header />
+      <MDXProvider components={shortcodes}>
       {children}
+      </MDXProvider>
       <Footer />
     </>
   );
