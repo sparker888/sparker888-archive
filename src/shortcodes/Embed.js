80a324d7df3e0ccae923a69ec15533442c5742cb
Stephen Parker <sparker888@gmail.com>
2021-09-25 19:40:45 -0400

    add Embed shortcode

--- src/shortcodes/Embed.js
+++ src/shortcodes/Embed.js
@@ -1,16 +1,12 @@
 import React from "react"
-import tw from "twin.macro";
 
 const Embed = ({ code }) => {
   return (
-    <CodeBlock>
+    <>
       {code}
-    </CodeBlock>
+    </>
   )
 }
 
 export default Embed
 
-const CodeBlock = tw.div`
-px-5 py-5
-`
