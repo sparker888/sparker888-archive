914cf4109aca4173de67477dc6fa78dd5f84cb76
Stephen Parker <sparker888@gmail.com>
2021-09-25 18:36:09 -0400

    updated blockquote

--- src/shortcodes/Blockquote.js
+++ src/shortcodes/Blockquote.js
@@ -1,105 +1,57 @@
+import tw from "twin.macro"
 import React from "react"
-// import { FiInfo } from "react-icons/fi"
-// import { TiWarningOutline } from "react-icons/ti"
-// import { GoQuote } from "react-icons/go"
-import styled from "styled-components"
 
-const Blockquote = ({ children, display }) => {
-  if (display === "warning")
-    return (
-      <Wrapper>
-        <div className="container warning">
-          {/* <TiWarningOutline className="icon" /> */}
-          {children}
-        </div>
-      </Wrapper>
-    )
-  if (display === "info")
-    return (
-      <Wrapper>
-        <div className="container info">
-/
-          {children}
-        </div>
-      </Wrapper>
-    )
-  if (display === "default") {
-    return (
-      <Wrapper>
-        <div className="container default">{children}</div>
-      </Wrapper>
-    )
-  } else {
-    return (
-      <Wrapper>
-        <div className="quote">
-          {/* <GoQuote className="quote-icon" /> */}
-          {children}
-        </div>
-      </Wrapper>
-    )
-  }
+const Blockquote = ({ children }) => {
+  return (
+    <Section>
+      <OuterWrapper>
+        <InnerWrapper>
+          <PatternWrapper>
+            <Svg
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 144 144"
+              aria-hidden="true"
+            >
+              <path
+                strokeWidth={2}
+                d="M41.485 15C17.753 31.753 1 59.208 1 89.455c0 24.664 14.891 39.09 32.109 39.09 16.287 0 28.386-13.03 28.386-28.387 0-15.356-10.703-26.524-24.663-26.524-2.792 0-6.515.465-7.446.93 2.327-15.821 17.218-34.435 32.11-43.742L41.485 15zm80.04 0c-23.268 16.753-40.02 44.208-40.02 74.455 0 24.664 14.891 39.09 32.109 39.09 15.822 0 28.386-13.03 28.386-28.387 0-15.356-11.168-26.524-25.129-26.524-2.792 0-6.049.465-6.98.93 2.327-15.821 16.753-34.435 31.644-43.742L121.525 15z"
+              />
+            </Svg>
+            <BlockQuote>
+              <BlockQuoteTextWrap>
+                <p>
+                    {children}
+                </p>
+              </BlockQuoteTextWrap>
+            </BlockQuote>
+          </PatternWrapper>
+        </InnerWrapper>
+      </OuterWrapper>
+    </Section>
+  )
 }
-const Wrapper = styled.blockquote`
-  .container {
-    padding: 2rem 1.5rem;
-    background: var(--clr-grey-10);
-    border-radius: var(--radius);
-    color: var(--clr-grey-1);
-    border-left: 3px solid var(--clr-grey-5);
-    position: relative;
-    margin: 2rem 0;
-  }
-  @media (min-width: 1170px) {
-    .container {
-      margin-left: -2rem;
-      margin-right: -2rem;
-    }
-  }
-  .icon {
-    position: absolute;
-    top: 0;
-    left: -3px;
-    background: var(--clr-white);
-    transform: translate(-50%, -50%);
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    border: 6px solid var(--clr-white);
-  }
-  .info {
-    background: var(--clr-primary-10);
-    color: var(--clr-primary-1);
-    border-color: var(--clr-primary-5);
-    .icon {
-      color: var(--clr-primary-5);
-    }
-  }
-  .warning {
-    background: #fffaeb;
-    color: #513c06;
-    border-color: #f7d070;
-    .icon {
-      color: #f7d070;
-    }
-  }
-  .quote {
-    @media (min-width: 576px) {
-      display: grid;
-      grid-template-columns: auto 1fr;
-      column-gap: 2rem;
-      align-items: center;
-    }
-    font-style: italic;
-    color: var(--clr-grey-5);
-    line-height: 1.8;
-    word-spacing: 3px;
-    font-size: 1.2rem;
-    margin: 2rem 0;
-    .quote-icon {
-      font-size: 6rem;
-      color: var(--clr-primary-5);
-    }
-  }
-`
+
 export default Blockquote
+
+const Section = tw.section`
+overflow-hidden
+`
+const OuterWrapper = tw.div`
+relative max-w-7xl mx-auto pt-20 pb-12 px-4 sm:px-6 lg:px-8 lg:py-20
+`
+const InnerWrapper = tw.div`
+relative lg:flex lg:items-center
+`
+const PatternWrapper = tw.div`
+relative lg:ml-10
+`
+const Svg = tw.div`
+absolute top-0 left-0 transform -translate-x-8 -translate-y-24 h-36 w-36 text-indigo-200 opacity-50
+`
+const BlockQuote = tw.blockquote`
+relative
+`
+const BlockQuoteTextWrap = tw.div`
+text-2xl leading-9 font-medium text-gray-900
+`
