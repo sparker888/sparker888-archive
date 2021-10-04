a06ae53e14c27d469247d0baa85419dc395dcdf1
Stephen Parker <sparker@gravitaldigital.com>
2021-10-04 08:00:58 -0400

    update aloha quote

--- src/pages/daily-photo.js
+++ src/pages/daily-photo.js
@@ -1,4 +1,4 @@
-import tw from "twin.macro"
+import tw, { styled } from "twin.macro"
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import { getImage } from 'gatsby-plugin-image';
@@ -52,8 +52,9 @@ bg-transparent
 const InnerWrapper = tw.div`
 absolute max-w-7xl top-1/4 bottom-1/2 md:top-1/4 md:bottom-1/4 mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-20
 `
-const H2 = tw.h2`
-text-4xl font-extrabold font-serif tracking-tight text-almond sm:text-5xl md:text-6xl
+const H2 = styled.h2`
+  text-shadow: 4px 4px 8px black;
+  ${tw`text-4xl font-extrabold font-serif tracking-tight text-almond sm:text-5xl md:text-6xl`}
 `
 const Span = tw.div`
 block
